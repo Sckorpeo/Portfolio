3ad9a066861cb8e4d441575bf507ba778979814f
Kieran B <kieranbru.web@gmail.com>
2022-11-04 17:14:58 -0400

    Added link to about

--- components/Nav.js
+++ components/Nav.js
@@ -2,7 +2,7 @@ const Nav = () => {
   return (
     <nav className='py-10 mb-8 flex justify-center'>
       <ul className='flex items-center w-[80%] justify-between'>
-        <li className=''>About</li>
+        <li className='#'>About</li>
         <li>
           <a href='#stack'>Stack</a>
         </li>
