8d7e4f73282df12a1a9e4dd80b15ce1f11074f72
Kieran B <kieranbru.web@gmail.com>
2022-11-04 13:29:49 -0400

    Completed main page mobile

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,9 @@
 import Head from "next/head";
+import Image from "next/image";
+
+import { AiFillLinkedin, AiFillGithub } from "react-icons/ai";
+
+import profileImg from "../public/port_img-removebg-preview.png";
 
 export default function Home() {
   return (
@@ -11,7 +16,40 @@ export default function Home() {
         />
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      <main></main>
+      <main className='px-10'>
+        <section className=' min-h-screen'>
+          <nav className='py-10 mb-12 flex justify-center'>
+            <ul className='flex items-center w-[80%] justify-between'>
+              <li className=''>About</li>
+              <li>Stack</li>
+              <li>Portfolio</li>
+            </ul>
+          </nav>
+          <div className='text-center p-10'>
+            <h2 className='text-4xl py-2'>Kieran Brugman</h2>
+            <h3 className='text-lg py-2 text-greenish-300'>
+              Fullstack Web Developer
+            </h3>
+            <p className='text-md py-5 leading-8 text-gray-800'>
+              Tax accountant turned programmer -- During my time working as a
+              tax accountant, I found myself enjoying debugging tax software for
+              my colleagues more than actually preparing taxes. This is what
+              started me down the long rabbit hole of programming.
+            </p>
+          </div>
+          <div className='text-5xl flex justify-center text-gray-600 gap-16 text-greenish-400'>
+            <a href='https://www.linkedin.com/in/kieran-brugman/'>
+              <AiFillLinkedin />
+            </a>
+            <a href='https://github.com/Sckorpeo'>
+              <AiFillGithub />
+            </a>
+          </div>
+          <div className='rounded-full relative overflow-hidden h-60 w-60 mt-20 mx-auto bg-gradient-to-b from-greenish-300'>
+            <Image src={profileImg} layout='fill' objectFit='cover' />
+          </div>
+        </section>
+      </main>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blueish: {
+          100: "#277BC0",
+        },
+        greenish: {
+          100: "#DEF5E5",
+          200: "#BCEAD5",
+          300: "#9ED5C5",
+          400: "#8EC3B0",
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
